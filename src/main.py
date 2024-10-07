ced5bf6139d0cc21a4ea8bce2fd5fddee21aea20
gewdvibes <gewdvibes@proton.me>
2024-10-06 20:59:58 -0400

    added textnode class and main function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    dummy = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(dummy)
+if __name__ == "__main__":
+    main()
